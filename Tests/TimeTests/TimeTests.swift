f98b8c171309ab344617a3b763509c5d3ec13720
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-08 21:12:21 +0300

    [Tests] Use new api

--- Tests/TimeTests/TimeTests.swift
+++ Tests/TimeTests/TimeTests.swift
@@ -3,9 +3,9 @@ import Test
 
 final class TimeTests: TestCase {
     func testNow() {
-        assertNotEqual(Time(), .now)
-        assertEqual(Time().seconds, Time.now.seconds)
-        assertTrue(Time() > Time(seconds: 1523558109, nanoseconds: 0))
+	    expect(Time() != .now)
+        expect(Time().seconds == Time.now.seconds)
+        expect(Time() > Time(seconds: 1523558109, nanoseconds: 0))
     }
 
     func testDuration() {
@@ -13,8 +13,8 @@ final class TimeTests: TestCase {
         let time: Time = .now
 
         let future = time + duration
-        assertEqual(future.nanoseconds, time.nanoseconds)
-        assertEqual(future.seconds, time.seconds + 5)
+        expect(future.nanoseconds == time.nanoseconds)
+        expect(future.seconds == time.seconds + 5)
     }
 
     func testInterval() {
@@ -22,51 +22,53 @@ final class TimeTests: TestCase {
         let location2: Time = location1 + 2.s
 
         let interval = Time.Interval(location1, location2)
-        assertEqual(interval.location, location1)
-        assertEqual(interval.duration, 2.s)
+        expect(interval.location == location1)
+        expect(interval.duration == 2.s)
 
         let interval1 = location1.interval(location2)
-        assertEqual(interval1, interval)
+        expect(interval1 == interval)
 
         let interval2 = location2.interval(location1)
-        assertEqual(interval2, interval)
+        expect(interval2 == interval)
     }
 
     func testEquatable() {
-        assertEqual(
-            Time(seconds: 1, nanoseconds: 2),
+        expect(
+            Time(seconds: 1, nanoseconds: 2)
+            ==
             Time(seconds: 1, nanoseconds: 2))
 
-        assertNotEqual(
-            Time(seconds: 1, nanoseconds: 2),
+        expect(
+            Time(seconds: 1, nanoseconds: 2)
+            !=
             Time(seconds: 1, nanoseconds: 3))
     }
 
     func testDescription() {
         let seconds = Time(seconds: 123, nanoseconds: 123_000_000)
-        assertEqual(seconds.description, "123.123 sec")
+        expect(seconds.description == "123.123 sec")
 
         let milliseconds = Time(seconds: 0, nanoseconds: 123_000_000)
-        assertEqual(milliseconds.description, "123 ms")
+        expect(milliseconds.description == "123 ms")
 
         let microseconds = Time(seconds: 0, nanoseconds: 123_000)
-        assertEqual(microseconds.description, "123 μs")
+        expect(microseconds.description == "123 μs")
 
         let nanoseconds = Time(seconds: 0, nanoseconds: 123)
-        assertEqual(nanoseconds.description, "123 ns")
+        expect(nanoseconds.description == "123 ns")
     }
 
     func testDouble() {
         let duration = Time.Duration(seconds: 123, nanoseconds: 321_000_000)
         let timeInterval = Double(duration)
-        assertEqual(timeInterval, 123.321)
+        expect(timeInterval == 123.321)
         let original = Time.Duration(timeInterval)
-        assertEqual(original, duration)
+        expect(original == duration)
     }
 
     func testFromString() {
         let time = Time("12/04/18 18:26:32", format: "%d/%m/%y %T")
-        assertEqual(time?.seconds, 1523557592)
-        assertEqual(time?.nanoseconds, 0)
+        expect(time?.seconds == 1523557592)
+        expect(time?.nanoseconds == 0)
     }
 }
